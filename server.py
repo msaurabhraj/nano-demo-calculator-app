f0f5da56f6f66c0bfa8f17df04c881fa156b0607
msaurabhraj <116434792+msaurabhraj@users.noreply.github.com>
2023-08-19 18:55:25 +0530

    updated routes for greet, add , substract

--- server.py
+++ server.py
@@ -1,19 +1,24 @@
-from flask import Flask
+from flask import Flask, request,json
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    res = json.loads(request.data)
+    x = {"result" : res['first'] + res['second']}
+
+    return x
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    res = json.loads(request.data)
+    x = {"result" : res['first']  - res['second']}
+    return x
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
