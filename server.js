aaf559907dd5d4923b041d05dd29d18944b5db0b
msaurabhraj <116434792+msaurabhraj@users.noreply.github.com>
2023-08-19 19:17:51 +0530

    updated the routes of add , greet, sub

--- server.js
+++ server.js
@@ -10,16 +10,22 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    first = req.body.first;
+    second = req.body.second;
+
+    res.json({ "result": first + second });
+
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    first = req.body.first;
+    second = req.body.second;
+    res.json({ "result": first - second});
 });
 
 app.use(baseUrl, baseRouter);
